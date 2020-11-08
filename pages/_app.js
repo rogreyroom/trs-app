acedb8d3b1cf3e83b3ceb1097f156a39aeeb07bd
Robert Adamczewski <robert@adamczewski.me>
2020-11-08 16:39:49 +0100

    feat: add global layout pattern and link global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import React from 'react'
+import App from 'next/app'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import 'normalize.css'
+import '../styles/globals.scss'
+
+// Based on https://adamwathan.me/2019/10/17/persistent-layout-patterns-in-nextjs/
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, router } = this.props
+
+    const getLayout = Component.getLayout || (page => page)
+
+    return getLayout(<Component {...pageProps}></Component>)
+  }
 }
 
 export default MyApp
