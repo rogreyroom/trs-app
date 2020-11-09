6c77c1bb073407cd383d087dd6362c3ebb2d2f6a
Robert Adamczewski <robert@adamczewski.me>
2020-11-09 17:13:31 +0100

    feat: add SvgEdit icon component and create EditIcon component

---
+++ components/IconButton.js
@@ -0,0 +1,46 @@
+import Link from 'next/link'
+import styled from 'styled-components'
+
+const StyledButton = styled.button.attrs(props => ({
+  size: props.size && `--${props.size}` || '--normal'
+}))`
+  --size: var(${props => props.size});
+
+  display: flex;
+  background: transparent;
+  box-shadow: none;
+  color: var(--c-white);
+  font-family: inherit;
+  line-height: var(--size);
+  font-size: var(--size);
+  text-align: center;
+  text-decoration: none;
+  border: none;
+  border-radius: var(--xs);
+  margin: 0;
+  padding: 0;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  cursor: pointer;
+  transition: all 250ms ease-in-out;
+
+  & > svg {
+    width: var(--size);
+    height: var(--size);
+  }
+
+  &:hover > svg   {
+    filter: var(--s-glow);
+    color: var(--c-accent);
+  }
+`
+
+export const IconButton = ({ children, href, size }) => {
+  return (
+    <Link href={href}>
+        <StyledButton size={size}>
+          { children }
+        </StyledButton>
+    </Link>
+  )
+}
