55cedeb43949fe21fc67fd7398609764d3084b5d
Robert Adamczewski <robert@adamczewski.me>
2020-11-09 08:18:58 +0100

    feat: rename layouts/app to layouts/AppLayout and create AppLayout component

---
+++ components/layouts/AppLayout.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+import { TopBar } from '../TopBar'
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  width: 100%;
+  min-height: 100vh;
+  background: var(--g-page);
+`
+const StyledMain = styled.main`
+  width: 100%;
+  min-height: 100%;
+  margin-top: var(--xs);
+  flex: 1;
+`
+
+
+const AppLayout = ({ children }) => {
+  return (
+    <AppContainer>
+      <TopBar />
+      <StyledMain>
+        { children }
+      </StyledMain>
+    </AppContainer>
+  )
+}
+
+export const getLayout = page => <AppLayout>{page}</AppLayout>
+
+export default AppLayout
