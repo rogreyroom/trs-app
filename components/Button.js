af66f01746b6750136c834e141381a13ddbbf22d
Robert Adamczewski <robert@adamczewski.me>
2020-11-16 17:08:32 +0100

    feat: move the styled component to the styled folder as a separate styled component file

--- components/Button.js
+++ components/Button.js
@@ -1,36 +1,5 @@
 import Link from 'next/link'
-import styled from 'styled-components'
-
-const StyledButton = styled.button`
-  display: flex;
-  background: var(--c-blue-03);
-  box-shadow: var(--s-button);
-  color: var(--c-white);
-  font-family: inherit;
-  line-height: 1;
-  font-size: var(--fs-text);
-  font-weight: var(--fw-normal);
-  text-align: center;
-  text-decoration: none;
-  border: none;
-  border-radius: var(--xs);
-  margin: 0;
-  padding: var(--s) var(--xl);
-  min-width: 130px;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  cursor: pointer;
-  transition: all 250ms ease-in-out;
-
-  &:hover {
-    box-shadow: var(--s-button-hover);
-    color: var(--c-accent);
-  }
-
-  & > :first-child {
-    margin-right: var(--xxs);
-  }
-`
+import { StyledButton } from './styled'
 
 export const Button = ({ children, href }) => {
   return (
