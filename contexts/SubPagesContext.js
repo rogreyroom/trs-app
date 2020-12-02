8d2abb2f674f633d8b1f601536fb871337bae801
Robert Adamczewski <robert@adamczewski.me>
2020-12-02 19:04:15 +0100

    feat: add context for sub pages

---
+++ contexts/SubPagesContext.js
@@ -0,0 +1,13 @@
+import { createContext, useState } from 'react'
+
+export const SubPagesContext = createContext()
+
+export const SubPagesProvider = ({ children }) => {
+  const [page, setPage] = useState(null)
+
+  return (
+    <SubPagesContext.Provider value={[page, setPage]}>
+        { children }
+    </SubPagesContext.Provider>
+  )
+}
