b78c1c3090e05b7e021dd373398195928b506e24
Robert Adamczewski <robert@adamczewski.me>
2020-12-03 20:22:02 +0100

    feat: add dashboard and logout buttons the the navbar

--- components/Header/Navbar.js
+++ components/Header/Navbar.js
@@ -1,19 +1,28 @@
 import styled from 'styled-components'
 import { Button } from './Button'
-import { SvgEes, SvgPdf } from '@/Icons'
+import { IconButton } from '@/components/IconButton'
+import { SvgDashboard, SvgEes, SvgPdf, SvgLogout } from '@/Icons'
 
 const StyledNav = styled.nav`
   display: flex;
   margin: 0;
 
-  > :last-child {
+  & button {
     margin-left: var(--l);
   }
+
+  > :first-child {
+      margin-left:  0;
+    }
 `
 
 export const Navbar = () => {
   return (
     <StyledNav>
+      <Button href='/dashboard/'>
+        <SvgDashboard />
+        Panel kierownika
+      </Button>
       <Button href='/dashboard/ees'>
         <SvgEes />
         SOP
@@ -22,6 +31,9 @@ export const Navbar = () => {
         <SvgPdf />
         Raporty
       </Button>
+      <IconButton href='/' size='l'>
+        <SvgLogout />
+      </IconButton>
     </StyledNav>
   )
 }
