c085641bed4d48fe69485bbfae9a6e442b5210d9
Robert Adamczewski <robert@adamczewski.me>
2020-12-03 20:18:36 +0100

    feat: add Logout and Dashboard svg icons

---
+++ components/Icons/Dashboard.js
@@ -0,0 +1,10 @@
+export const SvgDashboard = (props) => {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M4 17.333h10.667V4H4v13.333zM4 28h10.667v-8H4v8zm13.333 0H28V14.667H17.333V28zm0-24v8H28V4H17.333z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
