b76395f542f7eb1c42cdf2d9a1748d3119eb6843
Robert Adamczewski <robert@adamczewski.me>
2020-12-11 18:30:41 +0100

    feat: load initial server data to the dashboard and pass it to the context

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,16 +1,40 @@
 import Head from 'next/head'
+import { useContext } from 'react'
+import useSWR from 'swr'
+import { axios } from '@/lib/axios-config'
+import { DashboardContext } from '@/contexts/DashboardContext'
 import { getLayout } from '@/components/layouts/DashboardLayout'
+import { Board } from '@/components/Board'
 
-const Dashboard = ({ children }) => {
+const fetcher = url => axios.get(url).then(res => res.data)
+
+const Dashboard = ({ allEmployeesData }) => {
+  const [employees, setEmployees] = useContext(DashboardContext).data
+  const { data, error } = useSWR('api/employees', fetcher, { initialData: allEmployeesData })
+
+  if (error) return <h1>Something went wrong!</h1>
+  if (!data) return <h1>Loading...</h1>
+  setEmployees(data)
+
   return (
     <>
       <Head>
         <title>Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      { children }
+      <Board />
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const allEmployeesData = await fetcher('api/employees')
+
+  return {
+    props: {
+      allEmployeesData
+    }
+  }
 }
 
 Dashboard.getLayout = getLayout
