23b0a576c847d0b9e2b17b64d1e78c68fc32380d
Robert Adamczewski <robert@adamczewski.me>
2021-01-31 19:14:14 +0100

    feat: update dynamic imports paths, update props, add new dynamic pages

--- components/TheDashboard/ContentSection/index.js
+++ components/TheDashboard/ContentSection/index.js
@@ -1,34 +1,53 @@
 import dynamic from 'next/dynamic'
 import styled from 'styled-components';
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { SubPagesContext } from '@/contexts/SubPagesContext'
 
 export const StyledContentSection = styled.section`
 grid-area: content;
+display: flex;
+flex-direction: column;
 margin: 0;
 padding: var(--normal) var(--l);
 margin: var(--xl) var(--xxs) var(--xxs) var(--xl);
 background-image: var(--g-panel);
 box-shadow: var(--s-panel);
 color: var(--c-white);
+height: calc(100% - var(--xl));
 `
 
-const HolidayPage = dynamic(() => import('@/components/TheDashboard/SubPages/Holiday'), { ssr: false })
-const SickPage = dynamic(() => import('@/components/TheDashboard/SubPages/Sick'), { ssr: false })
-const LeavePage = dynamic(() => import('@/components/TheDashboard/SubPages/Leave'), { ssr: false })
-const RtsPage = dynamic(() => import('@/components/TheDashboard/SubPages/Rts'), { ssr: false })
-// const ReportsPage = dynamic(() => import('@/components/SubPages/Reports'), { ssr: false })
+const EditEmployeePage = dynamic(() => import('@/subPages/Employee/EditEmployee'), { ssr: false })
+const EmploymentStatusPage = dynamic(() => import('@/subPages/EmploymentStatus'), { ssr: false })
+const HolidayPage = dynamic(() => import('@/subPages/Holiday'), { ssr: false })
+const SickPage = dynamic(() => import('@/subPages/Sick'), { ssr: false })
+const LeavePage = dynamic(() => import('@/subPages/Leave'), { ssr: false })
+const RtsPage = dynamic(() => import('@/subPages/Rts'), { ssr: false })
+const ReportsPage = dynamic(() => import('@/subPages/Reports'), { ssr: false })
+const ResponsibilitiesPage = dynamic(() => import('@/subPages/Responsibilities'), { ssr: false })
 
-export const ContentSection = ({ employee }) => {
-  const [page,setPage] = useContext(SubPagesContext)
+export const ContentSection = ({ employeeId }) => {
+  const [page, setPage] = useContext(SubPagesContext).page
+  const [employeeChange, setEmployeeChange] = useState(null)
+
+  useEffect(() => {
+    if (employeeChange !== employeeId) {
+      setEmployeeChange(employeeChange => employeeId)
+      page !== 'rts' && setPage(page => 'rts')
+    }
+  }, [employeeId])
 
   return (
     <StyledContentSection>
-      { page === 'holiday' ? <HolidayPage employee={employee}/> : null }
-      { page === 'sick' ? <SickPage employee={employee}/> : null }
-      { page === 'leave' ? <LeavePage employee={employee}/> : null }
-      { page === 'rts' ? <RtsPage employee={employee}/> : null }
-      {/* { page === 'reports' ? <ReportsPage employee={employee}/> : null } */}
+
+      { page === 'edit' ? <EditEmployeePage employeeId={employeeId} /> : null }
+      { page === 'status' ? <EmploymentStatusPage employeeId={employeeId} /> : null }
+      { page === 'holiday' ? <HolidayPage employeeId={employeeId} /> : null }
+      { page === 'sick' ? <SickPage employeeId={employeeId} /> : null }
+      { page === 'leave' ? <LeavePage employeeId={employeeId} /> : null }
+      { page === 'rts' ? <RtsPage employeeId={employeeId} /> : null }
+      { page === 'reports' ? <ReportsPage employeeId={employeeId} /> : null }
+      { page === 'responsibilities' ? <ResponsibilitiesPage employeeId={employeeId} /> : null }
+
     </StyledContentSection>
   )
 }
