a4d536a6fb5dc82b7477b7ab0542cea8cab4ab1a
Robert Adamczewski <robert@adamczewski.me>
2021-01-31 19:38:30 +0100

    refactor: update styles to match new react requirements

--- components/TheDashboard/Main/index.js
+++ components/TheDashboard/Main/index.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components'
 
 export const StyledMain = styled.main.attrs({
-  template: props => props.dashboard && `'title title employee-nav employee-nav' 'details content content content' 'details content content content' ` || 'content',
-  columns: props => props.dashboard && '290px 1fr 1fr 1fr' || '1fr',
-  rows: props => props.dashboard && '50px 1fr' || '50px 1fr',
-  padding: props => props.dashboard && '0 var(--xl) var(--xl) var(--l)' || 'var(--xxl)'
+  GridTemplate: props => props.dashboard && `'title title employee-nav employee-nav' 'details content content content' 'details content content content' ` || `'title' 'content'`,
+  GridColumns: props => props.dashboard && '290px 1fr 1fr 1fr' || '1fr',
+  GridRows: props => props.dashboard && '50px 1fr' || '50px 1fr',
+  GridPadding: props => props.dashboard && '0 var(--xl) var(--xl) var(--l)' || 'var(--xxl)'
 })`
-  --grid-template: ${props => props.template};
-  --grid-columns:  ${props => props.columns};
-  --grid-rows:  ${props => props.rows};
-  --padding: ${props => props.padding};
+  --grid-template: ${props => props.GridTemplate};
+  --grid-columns:  ${props => props.GridColumns};
+  --grid-rows:  ${props => props.GridRows};
+  --padding: ${props => props.GridPadding};
 
   display: grid;
   grid-template-areas: var(--grid-template);
@@ -19,12 +19,14 @@ export const StyledMain = styled.main.attrs({
   margin: 0;
   width: 100%;
   height: 100%;
-
   position: relative;
+
+  & h1 {
+    align-self: center;
+  }
 `
 
 export const Main = ({ children, dashboard }) => {
-  console.log('Main is dashboard', dashboard);
   return (
     dashboard && (
     <StyledMain dashboard>
