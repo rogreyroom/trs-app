64a3627db0248b58186d669c8dc27bf52a3d3cf6
Robert Adamczewski <robert@adamczewski.me>
2021-01-31 19:20:46 +0100

    feat: update import paths, update router path, update dynamic page structure, change api calls

--- pages/dashboard/ees/index.js
+++ pages/dashboard/ees/index.js
@@ -2,11 +2,11 @@ import { useRouter } from 'next/router'
 import Head from 'next/head'
 import styled from 'styled-components'
 import { axios } from '@/lib/axios-config'
-import { getLayout } from '@/components/layouts/TopBarOnlyLayout'
-import { Title } from '@/components/common/Title'
-import { IconButton } from '@/components/common/Buttons'
-import { SvgEdit } from '@/Icons'
-import { StyledTable, StyledThead, StyledTbody, StyledTr, StyledTh, StyledTd } from '@/components/common/Table'
+import { getLayout } from '@/layouts/TopBarOnlyLayout'
+import { Title } from '@/common/Title'
+import { IconButton } from '@/common/Buttons'
+import { SvgEdit } from '@/icons'
+import { StyledTable, StyledThead, StyledTbody, StyledTr, StyledTh, StyledTd } from '@/common/Table'
 
 const Ees = ({ allEesData }) => {
   const router = useRouter()
@@ -37,7 +37,7 @@ const Ees = ({ allEesData }) => {
           {allEesData.map(({ _id, symbol, percent, description }) => (
             <StyledTr key={_id}>
               <StyledTd>
-                <IconButton size='m' onClickAction={() => router.push(`/ees/${encodeURIComponent(_id)}`)} >
+                <IconButton size='m' onClickAction={() => router.push(`/dashboard/ees/${encodeURIComponent(_id)}`)} >
                   <SvgEdit/>
                 </IconButton>
               </StyledTd>
