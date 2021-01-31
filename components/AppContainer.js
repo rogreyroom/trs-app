397ad9f00b988885c9a262c70782f26f7ecb90e7
Robert Adamczewski <robert@adamczewski.me>
2021-01-31 20:06:16 +0100

    refactor: update styles to match new react requirements

--- components/AppContainer.js
+++ components/AppContainer.js
@@ -1,19 +1,18 @@
 import styled from 'styled-components'
 
 export const AppContainer = styled.div.attrs({
-  template: props => props.login ? 'main' : props.dashboard ? `'header header' 'aside main'` : `'header' 'main'`,
-  columns: props => props.login ? '1fr' : props.dashboard ? '250px 1fr' : '1fr',
-  rows: props => props.login ? '1fr' : props.dashboard ? '104px 1fr' : '104px 1fr',
+  gridTemplate: props => props.login ? 'main' : props.dashboard ? `'header header' 'aside main'` : `'header' 'main'`,
+  gridColumns: props => props.login ? '1fr' : props.dashboard ? `250px 1fr` : '1fr',
+  gridRows: props => props.login ? '1fr' : props.dashboard ? '104px 1fr' : '104px 1fr',
 })`
-  --grid-template: ${props => props.template};
-  --grid-columns:  ${props => props.columns};
-  --grid-rows:  ${props => props.rows};
+  --grid-template: ${props => props.gridTemplate};
+  --grid-columns:  ${props => props.gridColumns};
+  --grid-rows:  ${props => props.gridRows};
 
   display: grid;
   grid-template-areas: var(--grid-template);
   grid-template-columns: var(--grid-columns);
   grid-template-rows: var(--grid-rows);
-  /* TODO: how to deal with this? */
   width: 100%;
   min-height: 100vh;
   justify-items: center;
