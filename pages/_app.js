0e61f704ab17dc832b0004d082ad7aba6bf644a2
Robert Adamczewski <robert@adamczewski.me>
2021-01-31 20:05:11 +0100

    feat: update the _app configuration file

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import App from 'next/app'
+import LoginLayout from '@/layouts/LoginLayout'
 
 import 'normalize.css'
+import 'react-modern-calendar-datepicker/lib/DatePicker.css'
 import '../styles/globals.scss'
 
 // Based on https://adamwathan.me/2019/10/17/persistent-layout-patterns-in-nextjs/
@@ -8,9 +10,9 @@ class MyApp extends App {
   render() {
     const { Component, pageProps, router } = this.props
 
-    const getLayout = Component.getLayout || (page => page)
+    const getLayout = Component.getLayout || ((page) => <LoginLayout children={page} />)
 
-    return getLayout(<Component {...pageProps}></Component>)
+    return getLayout(<Component {...pageProps} />)
   }
 }
 
