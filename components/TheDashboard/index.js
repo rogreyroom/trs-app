96a3de8862b273b9f437d1fad319f220b8bb1469
Robert Adamczewski <robert@adamczewski.me>
2021-01-31 19:55:20 +0100

    feat: update dashboard components

--- components/TheDashboard/index.js
+++ components/TheDashboard/index.js
@@ -1,97 +1,63 @@
-import useSWR from 'swr'
-import { axios } from '@/lib/axios-config'
-import { useContext, useState, useEffect } from 'react'
+import { useContext } from 'react'
 import { DashboardContext } from '@/contexts/DashboardContext'
 import { SubPagesProvider } from '@/contexts/SubPagesContext'
-import { Aside } from '@/components/TheDashboard/Sidebar'
-import { Main } from '@/components/TheDashboard/Main'
+import { Title } from "@/common/Title"
+import { AddEmployeePage } from '@/subPages/Employee/AddEmployee'
 import { Header } from './Header'
 import { MainNav } from './MainNav'
 import { DetailsSection } from './DetailsSection'
 import { ContentSection } from './ContentSection'
-import { SvgPeople, SvgDashboard } from '../Icons'
-import { TextButton, Button, IconButton } from '@/components/common/Buttons'
 
-const fetcher = url => axios.get(url).then(res => res.data)
+// TO be fixed
+import { Aside } from '@/components/TheDashboard/Sidebar'
+import { Main } from '@/components/TheDashboard/Main'
 
 export const Board = () => {
-  const [employees, setEmployees] = useContext(DashboardContext).data
   const [employee, setEmployee] = useContext(DashboardContext).employee
   const [addEmployeePage, setAddEmployeePage] = useContext(DashboardContext).add
+
+  console.log('Board addEmployeePage', addEmployeePage, 'employee', employee);
 
   if ( addEmployeePage ) {
     return (
     <>
       <Aside />
       <Main>
-        <h1>Add Employee page</h1>
-        <p>Display form to add new data</p>
-        <TextButton  isActive={true} onClickAction={() => console.log('Click Text')}>Text button</TextButton>
-        <br/>
-        <TextButton isUnderlined isActive={false}>Underlined text button</TextButton>
-        <br/>
-        <Button onClickAction={() => console.log('Click Text Button')}>Button with text only</Button>
-        <br/>
-        <Button isActive>
-          <SvgDashboard />
-          Button with icon in front of text
-        </Button>
-        <br/>
-        {/* <IconButton size='xxl'  onClickAction={() => console.log('Click Icon Button')}>
-          <SvgPeople />
-        </IconButton>
-        <br/>
-        <IconButton isActive>
-          <SvgPeople />
-        </IconButton> */}
+        <Title isWhite>Dodaj pracownika</Title>
+        <AddEmployeePage />
       </Main>
     </>
     )
   } else if (employee) {
-    const initialEmployeeData = employees.filter(emp => emp._id === employee)[0]
-    const { data, error } = useSWR(`api/employees/${employee}`, fetcher, { initialData: initialEmployeeData })
+    const {
+      _id,
+      name,
+      surname,
+      position,
+      juvenile_worker,
+      employment_status,
+      overdue_leave_amount,
+      assigned_leave_amount,
+      calendar,
+      employment_start_date,
+      employment_termination_date
+    } = employee
 
-  if (error) return <h1>Something went wrong!</h1>
-  if (!data) return <h1>Loading...</h1>
-
-    console.log('BOARD DATA', employees, employee, initialEmployeeData,  data);
-
-  const {
-    _id,
-    name,
-    surname,
-    position,
-    juvenile_worker,
-    employment_status,
-    overdue_leave_amount,
-    assigned_leave_amount,
-    calendar,
-    employment_start_date,
-    employment_termination_date
-  } = data
-
-
-  return (
-    <>
-      <Aside />
-      <Main dashboard>
-        <SubPagesProvider>
-          <Header name={`${name} ${surname}`} position={position} id={_id} juvenile={juvenile_worker} status={employment_status}  />
-          <MainNav employee={employee} />
-          <DetailsSection employeeCalendar={calendar} assignedLeaveDays={{overdue: overdue_leave_amount, assigned: assigned_leave_amount}} employmentDates={{start: employment_start_date, end: employment_termination_date}}/>
-          <ContentSection employee={employee} />
-        </SubPagesProvider>
-      </Main>
-    </>
-  )
-
-} else {
     return (
-    <>
-      <Aside />
-    </>
-  )
-}
+      <>
+        <Aside />
+        <Main dashboard>
+          <SubPagesProvider>
+            <Header employeeId={_id} name={`${name} ${surname}`} position={position} juvenile={juvenile_worker} status={employment_status}  />
+            <MainNav employeeId={_id} />
+            <DetailsSection employeeCalendar={calendar} assignedLeaveDays={{overdue: overdue_leave_amount, assigned: assigned_leave_amount}} employmentDates={{start: employment_start_date, end: employment_termination_date}}/>
+            <ContentSection employeeId={_id} />
+          </SubPagesProvider>
+        </Main>
+      </>
+    )
 
-
+  } else {
+      return <Aside />
+  }
 }
