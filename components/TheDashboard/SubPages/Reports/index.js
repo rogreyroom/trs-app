2b7eb7aa94d89c9d28aaed2d6bb6b71805d5c350
Robert Adamczewski <robert@adamczewski.me>
2021-01-31 19:32:51 +0100

    feat: update Reports component structure

--- components/TheDashboard/SubPages/Reports/index.js
+++ components/TheDashboard/SubPages/Reports/index.js
@@ -9,3 +9,17 @@
 //   - przycisk Anuluj (czyści formularz i przechodzi do panelu pracownika) __*5*9*1__
 //   - przycisk Zapisz (zapisuje dane w bazie, czyści formularz i przechodzi do panelu pracownika) __*5*9*2__
 //     )
+
+import { Title } from "@/common/Title"
+
+const ReportsPage = ({ employee }) => {
+  return (
+    <>
+      <Title isWhite>Reports sub page</Title>
+      <p>Employee: { employee }</p>
+      <p>Link list of all employee reports</p>
+    </>
+  )
+}
+
+export default ReportsPage
