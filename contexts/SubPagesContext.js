97cfbf8a03671933cc38f2dfc94fe77afb3c2edd
Robert Adamczewski <robert@adamczewski.me>
2021-01-31 18:46:11 +0100

    feat: add alias to pages context provider functions

--- contexts/SubPagesContext.js
+++ contexts/SubPagesContext.js
@@ -4,10 +4,9 @@ export const SubPagesContext = createContext()
 
 export const SubPagesProvider = ({ children }) => {
   const [page, setPage] = useState(null)
-  const [isClicked, setIsClicked] = useState(false)
 
   return (
-    <SubPagesContext.Provider value={[page, setPage], [isClicked, setIsClicked]}>
+    <SubPagesContext.Provider value={{ page: [page, setPage] }}>
         { children }
     </SubPagesContext.Provider>
   )
