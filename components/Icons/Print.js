719b4130d838157dc4e9b6c225039ade1bde6558
Robert Adamczewski <robert@adamczewski.me>
2021-02-12 16:34:49 +0100

    feat: add print icon

---
+++ components/Icons/Print.js
@@ -0,0 +1,17 @@
+export const SvgPrint = (props) => {
+  return (
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M25.333 10.667H6.667c-2.214 0-4 1.786-4 4v8H8V28h16v-5.333h5.333v-8c0-2.214-1.786-4-4-4zm-4 14.666H10.667v-6.666h10.666v6.666zm4-9.333C24.6 16 24 15.4 24 14.667c0-.734.6-1.334 1.333-1.334.734 0 1.334.6 1.334 1.334 0 .733-.6 1.333-1.334 1.333zM24 4H8v5.333h16V4z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
