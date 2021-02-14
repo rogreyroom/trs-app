67e5612c31625282ea18015201a42e1333020fd9
Robert Adamczewski <robert@adamczewski.me>
2021-02-14 21:26:05 +0100

    feat: create custom EvalAlert component to be used with react-confirm-alert

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import LoginLayout from '@/layouts/LoginLayout'
 
 import 'normalize.css'
 import 'react-modern-calendar-datepicker/lib/DatePicker.css'
+import 'react-confirm-alert/src/react-confirm-alert.css'
 import '../styles/globals.scss'
 
 // Based on https://adamwathan.me/2019/10/17/persistent-layout-patterns-in-nextjs/
