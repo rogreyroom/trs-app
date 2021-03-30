d7ace69312cc3b1c9bc9ac93bc2b3ec6a88b418b
Robert Adamczewski <robert@adamczewski.me>
2021-03-30 09:19:58 +0200

    feat: change login component and home page to authenticate user using api

--- renderer/components/TheLogin/index.js
+++ renderer/components/TheLogin/index.js
@@ -4,33 +4,53 @@ import {Input} from '@/common/Inputs';
 import {Button} from '@/common/Buttons';
 import {errorMessages} from '@/lib/errorMessages';
 import {useState} from 'react';
-
+import {axios} from '@/lib/axios-config';
+import styled from 'styled-components';
 import {StyledFormControlsWrapper, StyledForm} from '@/common/CommonWrappers';
+
+const Panel = styled.section`
+  background-image: var(--g-panel);
+  box-shadow: var(--s-panel);
+  padding: var(--xl);
+  display: flex;
+  flex-direction: column;
+  max-width: 50vw;
+
+  & h1 {
+    font-size: var(--xl);
+    color: var(--c-accent);
+    margin: 0;
+  }
+`;
 
 export const Login = () => {
   const router = useRouter();
   const {register, errors, handleSubmit, reset} = useForm();
   const [wrongLogin, setWrongLogin] = useState(false);
-
-  const adminName = 'tomek';
-  const adminPassword = 'fasttracker';
+  const [loading, setLoading] = useState(false);
 
   const onSubmit = (data, e) => {
-    console.log('Login user', data);
-
-    if (data.name === adminName && data.password === adminPassword) {
-      console.log('Zalogowany ', data.name, data.password);
-      router.push('/employees');
-    } else {
-      setWrongLogin((wrongLogin) => true);
-    }
+    const {name, password} = data;
+    const result = axios.post('/api/login', {name, password});
+    result
+      .then(({data}) => {
+        setLoading((loading) => true);
+        router.push('/employees');
+      })
+      .catch((err) => setWrongLogin((wrongLogin) => true));
     reset();
   };
 
+  if (wrongLogin) {
+    // TODO: add new component with return to login form button
+    return <h1>Nieprawidłowy login i/lub hasło!</h1>;
+  }
+  if (loading) {
+    return <h1>Loading data...............</h1>;
+  }
   return (
-    <>
-      {wrongLogin && <h1>Nieprawidłowy login i hasło!</h1>}
-
+    <Panel>
+      <h1>Login</h1>
       <StyledForm onSubmit={handleSubmit(onSubmit)}>
         <Input
           name="name"
@@ -54,6 +74,6 @@ export const Login = () => {
           <Button type="submit">Zaloguj</Button>
         </StyledFormControlsWrapper>
       </StyledForm>
-    </>
+    </Panel>
   );
 };
