1297609a19c87919c239303eb7406cb207d13c82
Robert Adamczewski <robert@adamczewski.me>
2021-03-30 11:14:10 +0200

    feat: add react-loader-spinner to the project

--- renderer/pages/_app.js
+++ renderer/pages/_app.js
@@ -4,6 +4,7 @@ import LoginLayout from '@/layouts/LoginLayout';
 import 'normalize.css';
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 import 'react-confirm-alert/src/react-confirm-alert.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import '../styles/globals.scss';
 
 // Based on https://adamwathan.me/2019/10/17/persistent-layout-patterns-in-nextjs/
